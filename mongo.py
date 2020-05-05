d6c53ce393db5e50be00d822afe2f1a2d6bf1e66
KMV <9117479@gmail.com>
2020-05-05 23:11:18 +0300

    Release

--- mongo.py
+++ mongo.py
@@ -1,13 +1,41 @@
 import csv
 import re
-import pprint
-from datetime import datetime
 from pymongo import MongoClient
 
 
-def connection(database):
+def read_data(csv_file, db):
+    mongo_db = client[db]
+    tickets_collection = mongo_db['tickets']
+
+    with open(csv_file, encoding='utf8') as csvfile:
+        rows_list = list()
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            row = dict(row)
+            row['Цена'] = int(row['Цена'])
+            rows_list.append(row)
+        tickets_collection.insert_many(rows_list)
+
+def find_cheapest(db):
+    mongo_db = client[db]
+    tickets_collection = mongo_db['tickets']
+
+    sorted_list = tickets_collection.find().sort('Цена', 1)
+    print(list(sorted_list))
+
+def find_by_name(name, database):в том числе – по подстроке, например "Seconds to"
+    mongo_db = client[database]
+    tickets_collection = mongo_db['tickets']
+
+    regex = re.compile(name)
+    sorted_list = tickets_collection.find({'Исполнитель': regex}).sort('Цена', 1)
+    for item in sorted_list:
+        print(item)
+
+if __name__ == '__main__':
     client = MongoClient()
-    db = client[database]
-    collection = db['concert']
-    return collection
+    read_data('artists.csv', 'test_db')
+    find_cheapest('test_db')
+    find_by_name('T', 'test_db')
 
